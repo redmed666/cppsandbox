931a055f8bbf803190fa417e3ad6ab30d85bf617
med <khemir.med@gmail.com>
2017-01-16 08:02:46 +0100

    test/: Added some basic unit tests

--- test/untittest_myapp.cpp
+++ test/untittest_myapp.cpp
@@ -1,5 +1,9 @@
 #include "gtest/gtest.h"
 #include "util/util.cpp"
+#include "curlcpp/curlcpp.cpp"
+#include "curlcpp/curlcppexception.cpp"
+#include "curlcpp/curlheaderscpp.cpp"
+#include "curlcpp/curlhandlercpp.cpp"
 
 TEST(TestUtilParseString, classicalString) {
     std::string input = "Un,dos,tres";
@@ -9,6 +13,20 @@ TEST(TestUtilParseString, classicalString) {
     for(int i = 0; i < output.size(); ++i){
         ASSERT_EQ((output[i]), (result[i]));
     }
+}
+
+TEST(TestCurlCpp, PerformRequestOK) {
+    CurlCpp::CurlCpp curlObject;
+    curlObject.setUrl("http://localhost:8000");
+    int responseCodeExpected = 200;
+    ASSERT_EQ(responseCodeExpected, curlObject.performRequest());
+}
+
+TEST(TestCurlCpp, PerformRequestNOK) {
+    CurlCpp::CurlCpp curlObject;
+    curlObject.setUrl("http://localhost:8001");
+    int responseCodeExpected = 0;
+    ASSERT_EQ(responseCodeExpected, curlObject.performRequest());
 }
 
 int main(int argc, char **argv) {
