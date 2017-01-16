fdf7194dbe3ce7575005942ae013684f7c807622
med <khemir.med@gmail.com>
2017-01-16 08:04:29 +0100

    server_test.py: Added a test server for the unit tests

---
+++ server_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import SimpleHTTPServer
+import SocketServer
+import logging
+
+PORT = 8000
+
+class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        logging.error(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = RequestHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+httpd.serve_forever()
